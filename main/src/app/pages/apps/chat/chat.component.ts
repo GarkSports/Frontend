7c558d463b134ce7c0f651dece84657d0869b13b
HaythemDaoud <haythem.daoud@esprit.tn>
2024-05-11 18:27:26 +0100

    chat

--- main/src/app/pages/apps/chat/chat.component.ts
+++ main/src/app/pages/apps/chat/chat.component.ts
@@ -1,25 +1,48 @@
-import { Component, ViewChild, ElementRef } from '@angular/core';
+import { Component, ViewChild, ElementRef, OnInit } from '@angular/core';
 import { messages } from './chat-data';
+import { ChatService } from 'src/app/services/chat.service';
+import { ChatContactDTO, ChatDTO } from 'src/models/chat.model';
+
 
 @Component({
   selector: 'app-chat',
   templateUrl: './chat.component.html',
   styleUrls: ['./chat.component.scss'],
 })
-export class AppChatComponent {
+export class AppChatComponent implements OnInit {
   sidePanelOpened = true;
   msg = '';
 
   // MESSAGE
   selectedMessage: any;
+  selectedDiscussion: any;
+  selectedContact: number;
+
 
   public messages: Array<any> = messages;
   // tslint:disable-next-line - Disables all
   // messages: Object[] = messages;
 
-  constructor() {
+  constructor(
+    public chatService: ChatService,
+  ) {
     this.selectedMessage = this.messages[0];
+    
+
   }
+  ngOnInit(): void {
+    
+    try {   
+      console.log('ngOnInit');
+       this.chatService.fetchContactList().subscribe((c: ChatContactDTO[]) => (this.chatService.contactList = c));
+
+    }
+    finally{
+      console.log('ngOnInit OK');
+    }
+    }
+
+  
 
   @ViewChild('myInput', { static: true }) myInput: ElementRef =
     Object.create(null);
@@ -29,20 +52,43 @@ export class AppChatComponent {
   }
 
   // tslint:disable-next-line - Disables all
-  onSelect(message: Object[]): void {
+  onSelect(message: number): void {
+    console.log('eeeeeeeeeeeee',message)
     this.selectedMessage = message;
+    this.chatService.getDiscussion(message).subscribe((d: ChatDTO[]) => (this.chatService.discussionList = d))
+
+      console.log('this.selectedMessage',this.chatService.discussionList);
+      this.selectedDiscussion=this.chatService.discussionList;
+      this.selectedContact=message;
+   
+  
+
   }
 
   OnAddMsg(): void {
     this.msg = this.myInput.nativeElement.value;
 
     if (this.msg !== '') {
-      this.selectedMessage.chat.push({
-        type: 'even',
-        msg: this.msg,
-        date: new Date(),
-      });
+      console.log('sending messager')
+      this.chatService.sendMessage(this.selectedContact, this.msg).subscribe(
+        () => {
+          console.log('Message sent successfully');
+          try {
+            this.chatService.getDiscussion(this.selectedContact).subscribe((d: ChatDTO[]) => (this.chatService.discussionList = d));
+          } finally {
+            console.log('this.selectedMessage',this.chatService.discussionList);
+          this.selectedDiscussion=this.chatService.discussionList;
+          }
+          
+        },
+        (error) => {
+          console.error('Error occurred while sending message:', error);
+          // Handle error if needed
+        }
+      );
+      
     }
+
 
     this.myInput.nativeElement.value = '';
   }
