cead28423dd838d8e9660d5966dd4924072d3ec3
HaythemDaoud <haythem.daoud@esprit.tn>
2024-06-09 16:37:22 +0100

    fix-min-val

--- main/src/app/pages/apps/comptabilite/update-benefice-depense/update-benefice-depense.component.ts
+++ main/src/app/pages/apps/comptabilite/update-benefice-depense/update-benefice-depense.component.ts
@@ -23,8 +23,8 @@ export class UpdateBeneficeDepenseComponent implements OnInit {
       type: ['', Validators.required],
       etat: ['', Validators.required],
       date: ['', Validators.required],
-      prixunite: ['', [Validators.required, Validators.min(0)]],
-      quantite: ['', [Validators.required, Validators.min(0)]],
+      prixunite: ['', [Validators.required, Validators.min(1)]],
+      quantite: ['', [Validators.required, Validators.min(1)]],
     });
   }
 
