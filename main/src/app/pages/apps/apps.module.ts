84fd60a91ae1917c8df90631cfe55e4500216691
yesserbenouhiba <yasser.benouhiba@esprit.tn>
2024-06-13 13:15:56 +0100

    remove ngximage

--- main/src/app/pages/apps/apps.module.ts
+++ main/src/app/pages/apps/apps.module.ts
@@ -149,7 +149,6 @@ import {AppComptabiliteComponent} from './comptabilite/comptabilite.component'
 import {AddBeneficeDepenseComponent} from './comptabilite/add-benefice-depense/add-benefice-depense.component';
 import {UpdateBeneficeDepenseComponent} from './comptabilite/update-benefice-depense/update-benefice-depense.component';
 import {FaIconComponent} from "@fortawesome/angular-fontawesome";
-import { NgxImageViewerModule } from '@simfyz/ngx-image-viewer';
 
 
 @NgModule({
@@ -181,7 +180,6 @@ import { NgxImageViewerModule } from '@simfyz/ngx-image-viewer';
     AppMonthlyEarningsTwoComponent,
     DefaultImagePipe,
     FaIconComponent,
-    NgxImageViewerModule
   ],
   exports: [TablerIconsModule],
   declarations: [
